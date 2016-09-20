3b2b80a6541c1ae5836eea123ef17b451592e0d4
zhyan5726 <zhyan5726@fiberhome.com>
2016-09-20 20:46:15 +0800

    2222

---
+++ src/yzh_test_git/dev1.java
@@ -0,0 +1,35 @@
+package yzh_test_git;
+
+public class dev1 {
+  
+  private int jk;
+  private int jk1;
+  private  int i;
+  private  int i1;
+  public int getJk() {
+    return jk;
+  }
+  public void setJk(int jk) {
+    this.jk = jk;
+  }
+  public int getJk1() {
+    return jk1;
+  }
+  public void setJk1(int jk1) {
+    this.jk1 = jk1;
+  }
+  public int getI() {
+    return i;
+  }
+  public void setI(int i) {
+    this.i = i;
+  }
+  public int getI1() {
+    return i1;
+  }
+  public void setI1(int i1) {
+    this.i1 = i1;
+  }
+  
+
+}
